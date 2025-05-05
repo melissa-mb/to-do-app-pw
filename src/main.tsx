3ded2d76ccb930f311cd0490447290f9d73261a5
H04306 <H04306@lab.aca>
2025-05-05 11:11:02 -0500

    mainpage

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import MainPage from './pages/MainPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <MainPage/>
   </StrictMode>,
 )
