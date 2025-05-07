4dfaa803611c6e299c8c3c88924fbd748d23cf56
A15207 <A15207@lab.aca>
2025-05-07 12:46:43 -0500

    formulario gestionable con variables de estado

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,23 +1,32 @@
 import { useState } from "react"
+interface FormularioProps {
+    agregar : (texto: string) => void
+}
 
-const Formulario = () => {
+const Formulario = (props: FormularioProps) => {
     const [textoTodo, setTextoTodo] = useState("")
 
-    const textoOnChange = (evt: React.ChangeEvent<HTMLInputElement>) =>{
+    const textoOnChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
         setTextoTodo(evt.currentTarget.value) // guarda lo que está actualmente en la variable en textoOnChange
     }
+
 
     return <div>
         {/*Formulario to-do's*/}
         <div className="row mt-4">
             <div className="col-md-10">
-                <input className="form-control" type="text" 
-                value={textoTodo} //establece que el formulario tenga "" inicialmente, pero no deja cambiar asi no mas
-                onChange={textoOnChange} // hace que en cada cambio se actualice el valor
+                <input className="form-control" type="text"
+                    value={textoTodo} //establece que el formulario tenga "" inicialmente, pero no deja cambiar asi no mas
+                    onChange={textoOnChange} // hace que en cada cambio se actualice el valor
                 />
             </div>
             <div className="col-md-2">
-                <button type="button" className="btn btn-primary">Agregar</button>
+                <button type="button" className="btn btn-primary" onClick={() => {
+                    props.agregar(textoTodo)
+                    console.log("Se debera guardar el todo:" + textoTodo)
+                }}>
+                    Agregar
+                </button>
             </div>
         </div>
     </div>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,20 +1,26 @@
+import { useState, type useSyncExternalStore } from "react"
 import Formulario from "../components/Formulario"
 import ListaTODOs, {type Todo} from "../components/ListaTODOs"
 import Navegacion from "../components/Navegacion"
 import Titulo from "../components/Titulo"
 
 const MainPage = () => {
-  const listaTODOs: Todo[] = [
-    {id : 1, nombre : "Ir al cine"}, 
-    {id : 2, nombre : "Limpiar cuarto"}, 
-    {id : 3, nombre : "Hacer desayuno"}
-  ]
+  const [listaTODOs,setListaTodos] = useState<Todo[]>([])
+
+
+  const agregarTodo = (texto: string) => {
+    listaTODOs.push({
+      id: listaTODOs.length+1,
+      nombre: texto
+    })
+    setListaTodos([...listaTODOs] /*crear una nueva lista y darle los elementosen listaTODOs*/)
+  }
 
   return <div className="container">
     <Titulo texto="To-Do App"/>
     <Navegacion />
-    <Formulario />
-    <ListaTODOs todos = {listaTODOs}/>
+    <Formulario agregar={agregarTodo}/>
+    <ListaTODOs todos = {listaTODOs}/> 
   </div>
 }
 
